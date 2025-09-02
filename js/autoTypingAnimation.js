7fd47c4a14007e1c0f458c60c118ef17c8be62b0
Hammad Ali <97672907+hammadali92@users.noreply.github.com>
2025-09-02 12:29:04 +0500

    design prtofolio

---
+++ js/autoTypingAnimation.js
@@ -0,0 +1,9 @@
+setTimeout(function() {
+    $('.about-info-inner').typed({
+      strings: [
+        "<span>Hi, I am Hammad Ali</span>  <br><br>  I am an experienced UI/UX Designer specializing in creating intuitive, user-friendly, and visually engaging digital experiences. With a strong background in design systems and modern tools, I have a passion for crafting high-performance, responsive, and accessible interfaces that enhance user satisfaction and business goals. I am a creative designer skilled in Figma, Adobe XD, Sketch, and Photoshop, with many years of experience developing professional designs for websites, landing pages, and web applications. I excel in wireframing, prototyping, and user journey mapping, ensuring each design is pixel-perfect, responsive, and aligned with UX best practices. I’m an expert in translating ideas into functional designs, delivering SEO-friendly, accessible, and user-focused solutions that are clean, scalable, and optimized for performance. My goal is to blend creativity with usability to deliver outstanding digital products "
+      ],
+      typeSpeed: 12,
+      contentType: 'html'
+    });
+  }, 100);
